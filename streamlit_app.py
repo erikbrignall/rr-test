dc508e2053560e7f90e2bd6eb85a4f9479619ce5
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2024-07-11 14:15:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,5 +25,5 @@ with st.form(key='my_form_to_submit'):
 if submit_button: 
     #inputs = ['cc','total_length','total_width','kerb_weight','torque','max_speed','acceleration','power','list_price','impact_parts_front']
     inputs = ['cc','total_length','total_width','kerb_weight','torque','max_speed']
-    print("your inputs are")
-    print(inputs)
+    st.write("your inputs are")
+    st.write(inputs)
