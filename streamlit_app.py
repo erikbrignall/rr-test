5635e0d3ee00dc5b5f4e1df607588919a0126bec
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2024-07-11 14:07:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,10 @@ with st.form(key='my_form_to_submit'):
     kerb_weight = st.number_input('Kerb Weight (kg)', min_value=100, max_value=10000, value=1600)
     torque = st.number_input('Torque (Nm)', min_value=1, max_value=10000, value=200)
     max_speed = st.slider('Max Speed (mph)', min_value=28, max_value=220, value=125)
-    acceleration = st.number_input('Acceleration 0-60', min_value=1, max_value=20, value=8)
-    power = st.number_input('Power (hp)', min_value=1, max_value=1500, value=150, step=0.1)
-    list_price = st.slider('List Price (£)', min_value=1000, max_value=10000000, value=30000)
-    impact_parts_front = st.slider('Impact Parts Cost Front', min_value=1, max_value=10, value=5)
+    #acceleration = st.number_input('Acceleration 0-60', min_value=1, max_value=20, value=8)
+    #power = st.number_input('Power (hp)', min_value=1, max_value=1500, value=150, step=0.1)
+    #list_price = st.slider('List Price (£)', min_value=1000, max_value=10000000, value=30000)
+    #impact_parts_front = st.slider('Impact Parts Cost Front', min_value=1, max_value=10, value=5)
     submit_button = st.form_submit_button(label='Submit')
 
 if submit_button: 
